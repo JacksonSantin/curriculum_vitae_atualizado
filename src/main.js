441beb8219f40f0639edb3fda7fc33303fd1886a
JacksonSantin <jackdhanyelsn@gmail.com>
2023-05-02 17:45:14 -0300

    :sparkles: feat: Configura projeto

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import '@mdi/font/css/materialdesignicons.css'
+
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
