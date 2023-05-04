f9290d187885cea9628823b62eaf70315e4e30f9
JacksonSantin <jackdhanyelsn@gmail.com>
2023-05-04 10:20:57 -0300

    :sparkles: feat: Adiciona redes sociais

--- src/components/navigation-drawer.vue
+++ src/components/navigation-drawer.vue
@@ -7,24 +7,48 @@
     permanent
   >
     <div class="icones my-2">
-      <div class="icones-topo">
+      <div class="icones-top">
         <v-icon
-          v-for="(item, idx) in iconesTopo"
+          v-for="(item, idx) in iconesTop"
           :key="idx"
           class="d-block text-center mx-auto mb-4"
           color="grey"
           size="24"
-          >{{ item.icone }}</v-icon
+          >{{ item.icon }}</v-icon
         >
       </div>
       <div class="icones-bottom">
-        <v-icon
-          v-for="(item, idx) in iconesBottom"
-          :key="idx"
-          class="d-block text-center mx-auto mb-4"
-          color="grey"
-          size="24"
-          >{{ item.icone }}</v-icon
+        <v-menu top open-on-hover :offset-x="true">
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon
+              v-bind="attrs"
+              v-on="on"
+              class="d-block text-center mx-auto mb-4"
+              color="grey"
+              size="24"
+              >mdi-account-circle-outline</v-icon
+            >
+          </template>
+
+          <v-list>
+            <v-list-item v-for="(item, index) in socialNetworks" :key="index">
+              <v-list-item-action>
+                <v-btn
+                  fab
+                  icon
+                  x-small
+                  :elevation="0"
+                  class="white--text font-weight-bold"
+                  @click="window.open(item.link)"
+                >
+                  <v-icon small> {{ item.icon }} </v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-icon class="d-block text-center mx-auto mb-4" color="grey" size="24"
+          >mdi-cog-outline</v-icon
         >
       </div>
     </div>
@@ -34,17 +58,28 @@
 <script>
 export default {
   data: () => ({
-    iconesTopo: [
-      { icone: "mdi-file-multiple-outline" },
-      { icone: "mdi-magnify" },
-      { icone: "mdi-source-branch" },
-      { icone: "mdi-play-outline" },
-      { icone: "mdi-grid-large" },
-      { icone: "mdi-dots-horizontal" },
+    iconesTop: [
+      { icon: "mdi-file-multiple-outline" },
+      { icon: "mdi-magnify" },
+      { icon: "mdi-source-branch" },
+      { icon: "mdi-play-outline" },
+      { icon: "mdi-grid-large" },
+      { icon: "mdi-dots-horizontal" },
     ],
-     iconesBottom: [
-      { icone: "mdi-account-circle-outline" },
-      { icone: "mdi-cog-outline" },
+    socialNetworks: [
+      {
+        icon: "mdi-linkedin",
+        link: "https://www.linkedin.com/in/jackson-dhanyel-santin",
+      },
+      { icon: "mdi-github", link: "https://github.com/JacksonSantin" },
+      {
+        icon: "mdi-instagram",
+        link: "https://www.instagram.com/jackson_santin",
+      },
+      {
+        icon: "mdi-facebook",
+        link: "https://www.facebook.com/jackson.santin.52",
+      },
     ],
   }),
 };
